6b64e0f58cba89efc9e34ffd57d2edef758191ca
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-17 18:53:38 +0200

    Add files via upload

---
+++ Rs7Audi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudiAbstractFactory
+{
+	class Rs7Audi: Audi
+	{	
+		public override string Brand 
+		{
+			get { return "Audi Rs7"; }
+		}
+		public override string Model
+		{
+			get { return "Rs7"; }
+		}
+		public override string EngineType
+		{
+			get { return "Benzin"; }
+		}
+		public override string Color
+		{
+			get { return "Grey"; }
+		}
+
+		public override void Print()
+		{
+			Console.WriteLine($"The brand of the car is: {Brand}");
+			Console.WriteLine($"The model of the car is: {Model}");
+			Console.WriteLine($"The engine type of the car is: {EngineType}");
+			Console.WriteLine($"The color of the car is: {Color}");
+		}
+	}
+}
