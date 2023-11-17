26b012b7b420848996ec82f07ef6446a717ac933
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-17 18:52:45 +0200

    Add files via upload

---
+++ Audi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudiAbstractFactory
+{
+	abstract class Audi
+	{
+		public abstract string  Brand { get; }
+		public abstract string Model { get; }
+		public abstract string EngineType { get; }
+		public abstract string Color { get; }
+		public abstract void Print();
+	}
+}
