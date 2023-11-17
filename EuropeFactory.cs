b3cad1d84ebff99b911cb185c099d9d5bca075d3
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-17 18:53:19 +0200

    Add files via upload

---
+++ EuropeFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudiAbstractFactory
+{
+	class EuropeFactory:AudiFactory
+	{
+		public override Audi CreateModel()
+		{
+			return new A8Audi();
+		}
+
+	}
+}
