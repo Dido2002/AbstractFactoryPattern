496bedc8406ca857b53b1166854cd785cafdc117
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-25 23:02:14 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Runtime.Intrinsics.Arm;
-using System.Security.Cryptography.X509Certificates;
-
-namespace AudiAbstractFactory
+﻿namespace ToyotaAbstractFactory 
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.ReadKey();	
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            ToyotaFactory myToyota = new JapanFactory();
+            var prius = myToyota.CreateModel();
+            prius.Print();
 
-			AudiFactory myAudi = new EuropeFactory();
-			var audiA8 = myAudi.CreateModel();
-			audiA8.Print();
-		
-			myAudi = new USAFactory();
-			var audiRs7 = myAudi.CreateModel();
-			audiRs7.Print();
+            Console.WriteLine();
+                
+            myToyota = new EuropeFactory();  
+            var corolla = myToyota.CreateModel();  
+            corolla.Print();
 
-			
-		}
-	}
+            Console.ReadKey();
+        } 
+    }
 }
