468e32393ec58933651cf54f304fbc46f6ec7f3d
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-25 23:02:26 +0200

    Add files via upload

---
+++ Toyota.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyotaAbstractFactory
+{
+    abstract class Toyota // AbstractProduct
+    {
+        public abstract string Name { get; }
+        public abstract string Model { get; }
+        
+        public abstract string EngineType { get; }
+
+        public abstract string Color { get; }
+
+        public abstract void Print();
+    }
+}
