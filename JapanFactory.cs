7732a4d02459a126e8f9b0de6f64316c0eff2815
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-25 23:01:37 +0200

    Add files via upload

---
+++ JapanFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyotaAbstractFactory
+{
+    class JapanFactory:ToyotaFactory    // ConctreteFactory1
+    {
+        public override Toyota CreateModel()
+        {   
+            return new PriusToyota();   //Concrete Product1
+        }
+
+    }
+}
